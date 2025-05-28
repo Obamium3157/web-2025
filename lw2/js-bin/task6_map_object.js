b3077861e442e9d8b27b380f01c880de89276260
obamium3157 <p_zhdanov@icloud.com>
2025-05-29 02:20:13 +0300

    lw 10 ez

--- lw2/js-bin/task6_map_object.js
+++ lw2/js-bin/task6_map_object.js
@@ -1,11 +1,8 @@
 function mapObject(obj, callback) {
     const result = {};
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            result[key] = callback(obj[key]);
-        }
-    }
-
+    Object.keys(obj).forEach(key => {
+        result[key] = callback(obj[key]);
+    });
     return result;
 }
 
--- lw2/js-bin/task8_map_and_filter.js
+++ lw2/js-bin/task8_map_and_filter.js
@@ -1,5 +1,3 @@
-function triple_and_filter(arr) {
-    return arr.map(x => x * 3).filter(x => x > 10);
-}
+let triple_and_filter = (arr) => arr.map(x => x * 3).filter(x => x > 10);
 
 console.log(triple_and_filter([2, 5, 8, 10, 3]));
